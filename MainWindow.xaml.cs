61f9f8474e5d9cfcb03ffece2a39dbaccafc817f
Rafal Chabasinski <r.chabasinski@student.mini.pw.edu.pl>
2020-11-08 22:10:38 +0100

    file loading workz
    Signed-off-by: Rafal Chabasinski <r.chabasinski@student.mini.pw.edu.pl>

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -4,11 +4,12 @@ using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Xml.Serialization;
+using System.Windows.Input;
 using Microsoft.Win32;
 using Tetris.Algorithms;
 using Tetris.Graphics;
@@ -19,12 +20,26 @@ namespace Tetris
 {
     public partial class MainWindow
     {
+        private (List<Shape> shapes, int shapeSize) _generatedShapes;
+        private Stopwatch _sw;
+
+        private readonly Dictionary<int, TetrisFitter> _tagToFitter = new Dictionary<int, TetrisFitter>
+        {
+            {0, new HeuristicTetrisFitter()},
+            {1, new OptimalTetrisFitter()}
+        };
+
+        private CancellationTokenSource _tokenSource;
+
         public MainWindow()
         {
             InitializeComponent();
             ResolutionSurface = new PaintSurface(ResolutionImage);
             InputSurface = new PaintSurface(InputImage);
         }
+
+        private PaintSurface ResolutionSurface { get; }
+        private PaintSurface InputSurface { get; }
 
         public void Test()
         {
@@ -34,20 +49,11 @@ namespace Tetris
                     .Select(s => s.GenerateAllCuts().Count).Max())).ToList();
         }
 
-        private PaintSurface ResolutionSurface { get; }
-        private PaintSurface InputSurface { get; }
-
-        private Dictionary<int, TetrisFitter> _tagToFitter = new Dictionary<int, TetrisFitter>()
-        {
-            {0, new HeuristicTetrisFitter()},
-            {1, new OptimalTetrisFitter()},
-        };
-
         private async void ExecuteAlgorithmClick(object sender, RoutedEventArgs e)
         {
             ShowOverlay();
 
-            int tag = int.Parse((sender as Button)?.Tag.ToString()!);
+            var tag = int.Parse((sender as Button)?.Tag.ToString()!);
             var tetrisFitter = _tagToFitter[tag];
             var shapes = _generatedShapes.shapes;
             var indexToColor = shapes.ToDictionary(s => s.Index, s => s.Color);
@@ -76,8 +82,8 @@ namespace Tetris
         {
             ShowOverlay();
 
-            int shapeCount = ShapeCount.Value.GetValueOrDefault();
-            int shapeSize = ShapeSize.Value.GetValueOrDefault();
+            var shapeCount = ShapeCount.Value.GetValueOrDefault();
+            var shapeSize = ShapeSize.Value.GetValueOrDefault();
 
             try
             {
@@ -121,10 +127,6 @@ namespace Tetris
             MainDisplay.IsEnabled = false;
         }
 
-        private (List<Shape> shapes, int shapeSize) _generatedShapes;
-        private CancellationTokenSource _tokenSource;
-        private Stopwatch _sw;
-
         private void CancelOperationClick(object sender, RoutedEventArgs e)
         {
             _tokenSource.Cancel();
@@ -132,44 +134,43 @@ namespace Tetris
 
         private void LoadPentominoes(object sender, RoutedEventArgs e)
         {
-            var dll = new FileInfo(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            var dll = new FileInfo(Assembly.GetExecutingAssembly().Location);
             var filesToLoadDir = dll.Directory.Parent.Parent.Parent.FullName + "\\FilesToLoad";
-            
-            OpenFileDialog ofd = new OpenFileDialog
+
+            var ofd = new OpenFileDialog
             {
                 InitialDirectory = filesToLoadDir,
                 Title = "Load pentominoes",
                 FilterIndex = 2,
                 RestoreDirectory = false,
                 CheckFileExists = true,
-                CheckPathExists = true,
+                CheckPathExists = true
             };
 
             var result = ofd.ShowDialog();
-            const int fileShapeSize = 5;
 
             if (result.HasValue && result.Value)
             {
                 var file = File.ReadAllText(ofd.FileName);
-                var split = file.Split(new[] {' ', '\r', '\n'}, StringSplitOptions.RemoveEmptyEntries);
-                var shapeCounts = split.Take(18).Select(int.Parse).ToList();
-                var pentominoes = ShapeGenerator.GenerateOneSidedShapes(fileShapeSize, CancellationToken.None);
-
-                var shapes = new List<Shape>();
-                int shapeIndex = 0;
-                for (int i = 0; i < shapeCounts.Count; i++)
+                var shapeLists = Parser.Parse(file);
+                LoadedSetups.Children.Clear();
+                for (var i = 0; i < shapeLists.Count; i++)
                 {
-                    var oneSidedShape = pentominoes[i];
-                    for (int j = 0; j < shapeCounts[i]; j++)
+                    var shapes = shapeLists[i];
+                    var button = new Button()
                     {
-                        shapes.Add(new Shape(shapeIndex++, oneSidedShape, fileShapeSize));
-                    }
+                        Content = i+1,
+                        Width = 45
+                    };
+                    button.Click += (o, args) =>
+                    {
+                        _generatedShapes = shapes;
+                        ResolutionSurface.Clear();
+                        DisplayMethods.DisplayInputShapes(shapes.shapeSize, shapes.shapes, InputSurface);
+                        EnableButtons();
+                    };
+                    LoadedSetups.Children.Add(button);
                 }
-
-                _generatedShapes = (shapes, fileShapeSize);
-                ResolutionSurface.Clear();
-                DisplayMethods.DisplayInputShapes(fileShapeSize, shapes, InputSurface);
-                EnableButtons();
             }
         }
     }
---
+++ Services/Parser.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using Tetris.Shapes;
+
+namespace Tetris.Services
+{
+    public static class Parser
+    {
+        private static readonly Dictionary<int, int> pentominoNumberToPentominoGenerator = new Dictionary<int, int>
+        {
+            {6, 0}, {9, 1}, {12, 2}, {5, 3}, {11, 4}, {7, 5}, {2, 6}, {13, 7}, {17, 8}, {4, 9}, {15, 10},
+            {10, 11}, {16, 12}, {1, 13}, {8, 14}, {3, 15}, {18, 16}, {14, 17}
+        };
+
+        private static readonly Dictionary<int, int> hexominoNumberToHexominoGenerator = new Dictionary<int, int>
+        {
+            {1, 46}, {2, 37}, {3, 45}, {4, 43}, {5, 47}, {6, 24}, {7, 35}, {8, 17}, {9, 20}, {10, 14}, {11, 44},
+            {12, 42}, {13, 27}, {14, 33}, {15, 36}, {16, 59}, {17, 57}, {18, 48}, {19, 18}, {20, 49}, {21, 40}, {22, 3},
+            {23, 51}, {24, 22}, {25, 53}, {26, 50}, {27, 31}, {28, 11}, {29, 41}, {30, 26}, {31, 34}, {32, 1}, {33, 19},
+            {34, 21}, {35, 54}
+        };
+
+        public static List<(List<Shape> shapes, int shapeSize)> Parse(string file)
+        {
+            var result = new List<(List<Shape> shapes, int shapeSize)>();
+            
+            var lines = file.Split(new[] {'\r', '\n'}, StringSplitOptions.RemoveEmptyEntries);
+
+            var shapeSize = -1;
+            for (var k = 0; k < lines.Length; k++)
+            {
+                var modI = k % 3;
+                var shapesOfSizeCount = shapeSize == 5 ? 18 : 35;
+                var line = lines[k];
+                switch (modI)
+                {
+                    case 0:
+                        shapeSize = int.Parse(line);
+                        break;
+                    case 2:
+                        var shapeCounts = line.Split(' ', StringSplitOptions.RemoveEmptyEntries).Take(shapesOfSizeCount).Select(int.Parse).ToList();
+                        var shapes = new List<Shape>();
+                        var shapeIndex = 0;
+                        for (var i = 0; i < shapeCounts.Count; i++)
+                        {
+                            var pentominoIndex = shapeSize == 5
+                                ? pentominoNumberToPentominoGenerator[i + 1]
+                                : hexominoNumberToHexominoGenerator[i + 1];
+                            var polyominos = ShapeGenerator.GenerateOneSidedShapes(shapeSize, CancellationToken.None);
+                            var oneSidedShape = polyominos[pentominoIndex];
+                            for (var j = 0; j < shapeCounts[i]; j++)
+                                shapes.Add(new Shape(shapeIndex++, oneSidedShape, shapeSize));
+                        }
+                        result.Add((shapes, shapeSize));
+                        break;
+                }
+            }
+
+            return result;
+        }
+    }
+}
